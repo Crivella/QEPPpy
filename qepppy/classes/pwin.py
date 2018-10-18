c46d99c6d46bfaea6e54c2df3cf1dcdb8a978dea
Crivella <davidecrivella@yahoo.it>
2018-10-18 12:41:28 +0200

    Several progress to operating pwin and strcture classes

--- qepppy/classes/pwin.py
+++ qepppy/classes/pwin.py
@@ -1,9 +1,10 @@
 import sys
+import numpy as np
 
 from qepppy.classes.structure import structure as structure, bravais_index as bi
 
 class pwin( ):
-	def __init__( self, stc=""):
+	def __init__( self, stc=None):
 		#if not isinstance( stc, structure):
 		#	raise Exception( "Must pass a valid strucutre")
 		self._d = {
@@ -21,13 +22,13 @@ class pwin( ):
 				"dt" : "",
 				"outdir" : "./tmp",
 				"wfcdir" : "",
-				"prefix" : "",
+				"prefix" : "***",
 				"lkpoint_dir" : "",
 				"max_seconds" : "",
 				"etot_conv_thr" : "",
 				"forc_conv_thr" : "",
 				"disk_io" : "",
-				"pseudo_dir" : "",
+				"pseudo_dir" : "***",
 				"tefield" : "",
 				"dipfield" : "",
 				"lelfield" : "",
@@ -40,22 +41,27 @@ class pwin( ):
 				"gate" : ""
 			},
 			"SYSTEM":{
-				"ibrav" : "",
-				"celldm" : "",
+				"ibrav" : "***",
+				"celldm(1)" : "***",
+				"celldm(2)" : "",
+				"celldm(3)" : "",
+				"celldm(4)" : "",
+				"celldm(5)" : "",
+				"celldm(6)" : "",
 				"A" : "",
 				"B" : "",
 				"C" : "",
 				"cosAB" : "",
 				"cosAC" : "",
 				"cosBC" : "",
-				"nat" : "",
-				"ntyp" : "",
+				"nat" : "***",
+				"ntyp" : "***",
 				"nbnd" : "",
 				"tot_charge" : "",
 				"starting_charge" : "",
 				"tot_magnetization" : "",
 				"starting_magnetization" : "",
-				"ecutwfc" : "",
+				"ecutwfc" : "***",
 				"ecutrho" : "",
 				"ecutfock" : "",
 				"nr1" : "",
@@ -193,46 +199,100 @@ class pwin( ):
 				"cell_dofree" : ""
 			}
 		}
+		self.stc = None
+		if stc:
+			if isinstance( stc, structure):
+				self._add_stc_( stc)
+			else:
+				raise Exception( "stc is not an instance of structure")
 
 	def __str__( self):
-		self.print()
+		self.print_in()
 		return ""
 
-	def print( self, fname=""):
-		if fname:
-			f = open( fname, "w+")
-			pfunc = f.write
-		else:
-			pfunc = sys.stdout.write
-		longest = 0
-		for namelist in self._d["nl"]:
-			for el, v in self._d[namelist].items():
-				if v != "":
-					app = len(el)
-					if longest < app: longest = app
-		longest += 2
+	def __iadd__(self, other):
+		if isinstance( other, structure):
+			self._add_stc_( other)
 
-		for namelist in self._d["nl"]:
-			pfunc( "&{}\n".format(namelist))
-			for el, v in self._d[namelist].items():
-				if v != "":
-					pfunc( "{0:>{1}} = ".format( el, longest))
-					if isinstance( v, str):
-						pfunc( "'{0}'".format( v))
-					if isinstance( v, bool):
-						if v: pfunc( ".TRUE.")
-						else: pfunc( ".FALSE.")
-					else:
-						if isinstance( v, (int, float)):
-							pfunc( v)
-					pfunc( " ,\n")
-			pfunc("/\n")
+		return self
+
+	def _add_stc_( self, stc):
+		self.stc = stc
+		if isinstance( stc.bravais_n, int):
+			self._d["SYSTEM"]["ibrav"] = stc.bravais_n
+		else:
+			if not stc.a:
+				raise Exception("Must pass a valid cel structure")
+			self._d["SYSTEM"]["ibrav"] = 0
+
+		self._d["SYSTEM"]["celldm(1)"] = stc.lp
+		if self._d["SYSTEM"]["ibrav"] == 0:
+			if not isinstance( stc.a, np.ndarray):
+				raise Exception( "Basis vector must be set with ibrav = 0")
+
+		if stc.atom_spec_n != len( stc.atom_spec):
+			raise Exception( "Invalide structure data, ntyp does not match")
+		self._d["SYSTEM"]["ntyp"] = stc.atom_spec_n
+		self._d["SYSTEM"]["nat"] = len( stc.atoms)
 
 		return
 
-"""
-if __name__ == "__main__":
-	a = pwin()
-	a.print()
-"""
+	def print_in( self, fname=""):
+		if not self.stc:
+			raise Exception("No valid cell structure for the input file")
+
+		#Define output 
+		if fname:
+			f = open( fname, "w+")
+		else:
+			f = sys.stdout
+
+		#Checks
+		longest = 0
+		check_mand = False
+		nl = self._d["nl"].copy()
+		for namelist in self._d["nl"]:
+			#Check for unused namelist (does not print it)
+			check_used = False
+			for el, v in self._d[namelist].items():
+				if v != "":
+					check_used = True
+					app = len(el)
+					#Adjust lenght of written parameters to have the = all in column
+					if longest < app: longest = app
+					#Check for mandatory parameters
+					if v == "***":
+						check_mand = True
+						sys.stderr.write( "Mandatory input parameter {} in namelist {} not set.\n".format( el, namelist))
+			if not check_used:
+				nl.pop( nl.index(namelist))
+		if check_mand:
+			return
+		longest += 2
+
+		#Write all the used namlists/parameters
+		for namelist in nl:
+			f.write( "&{}\n".format(namelist))
+			for el, v in self._d[namelist].items():
+				if v != "":
+					f.write( "{0:>{1}} = ".format( el, longest))
+					if isinstance( v, str):
+						f.write( "'{0}'".format( v))
+					if isinstance( v, bool):
+						if v: f.write( ".TRUE.")
+						else: f.write( ".FALSE.")
+					else:
+						if isinstance( v, (int, float)):
+							f.write( "{}".format( v))
+					f.write( " ,\n")
+			f.write("/\n\n")
+
+		#Write the structure data
+		f.write( self.stc.__str__())
+
+		if fname:
+			f.close()
+
+		return
+
 
--- qepppy/classes/structure.py
+++ qepppy/classes/structure.py
@@ -3,24 +3,74 @@ import re
 import numpy as np
 import xml.etree.ElementTree as ET
 
-bravais_index={	'0':'free', '1':'simple cubic (sc)', '2':'face-centered cubic (fcc)', '3':'body-centered cubic (bcc)', \
-		'-3':'bcc more symm. axis', '4':'hexagonal', '5':'trigonal', '-5':'trigonal <111>', '6':'simple tetragonal (st)', \
-		'7':'body-centered tetragonal (bct)', '8':'orthorombic P', '9':'base-centered orthorombic (bco)', '-9':'as 9 different axis', \
-		'10':'face-centered orthorombic', '11':'body-centered orthorombic', '12':'monoclinic P', '-12':'as 12 unique axis', \
-		'13':'base-centered monoclinic', '14':'triclinic'}
+bravais_index={	'0':'free', '1':'simple cubic (sc)', '2':'face-centered cubic (fcc)', '3':'body-centered cubic (bcc)',
+	'-3':'bcc more symm. axis', '4':'hexagonal', '5':'trigonal', '-5':'trigonal <111>', '6':'simple tetragonal (st)',
+	'7':'body-centered tetragonal (bct)', '8':'orthorombic P', '9':'base-centered orthorombic (bco)', 
+	'-9':'as 9 different axis', '10':'face-centered orthorombic', '11':'body-centered orthorombic', '12':'monoclinic P',
+	'-12':'as 12 unique axis', '13':'base-centered monoclinic', '14':'triclinic'}
 
 class structure:
 	__name__ = "structure"
-	def __init__( self, fname=""):
-		if not fname:
-			raise Exception( "Must initialize class giving the name of the .xml file")
-		if not os.path.isfile( fname):
-			raise IOError( "File '{}' does not exist".format( fname))
-		self.fname = fname
-		self._parse_xml_()
+	def __init__( self, fname="", **kwargs):
+		#if not fname:
+		#	raise Exception( "Must initialize class giving the name of the .xml file")
+		self.bravais_n = None
+		self.bravais = None
+		self.lp = None
+		self.a = None
+		self.b = None
+		self.atoms = None
+		self.atom_spec = None
+		self.atom_spec_n= None
+		self.symm = None
 
-	def __str__( self):
-		return ''
+		if fname:
+			if not os.path.isfile( fname):
+				raise IOError( "File '{}' does not exist".format( fname))
+			self.fname = fname
+			self._parse_xml_()
+		else:
+			for k, v in kwargs.items():
+				#print( k, v)
+				if k in self.__dict__:
+					self.__dict__[k] = v
+				else:
+					raise Exception(" unrecognized keyword argument {}".format( k))
+
+
+		return		
+
+	def __str__( self, info=0):
+		t=""
+		if self.bravais_n == 0 or info > 0:
+			t += "CELL_PARAMETERS"
+			for l in self.a:
+				for e in l:
+					t += "{}".format(e)
+				t += "\n"
+			t += "\n\n"
+
+		if self.atom_spec:
+			t += "ATOMIC_SPECIES\n"
+			for s in self.atom_spec:
+				t += "{:6}{:12.4f}  {}".format(s['name'],s['mass'],s['pfile'])
+			t += "\n\n"
+
+		if self.atoms:
+			t += "ATOMIC_POSITIONS\n"
+			for a in self.atoms:
+				t += "{:4}  ".format(a['name'])
+				for c in a['coord']:
+					t += "{:10.5f}".format(c)
+				t += "\n"
+			t += "\n"
+		else:
+			raise Exception( "No atom specified")
+
+		if info == 1:
+			t += "dbg info"
+
+		return t
 
 	def __getattr__( self, key):
 		if key in self.__dict__:
@@ -43,16 +93,30 @@ class structure:
 	def _parse_xml_( self):
 		root = ET.parse( self.fname).getroot()
 
-		self.bravais = root.find("output//atomic_structure").get("bravais_index")
-		if self.bravais in bravais_index:
-			self.bravais = bravais_index[ self.bravais]
+		self.bravais_n = root.find("output//atomic_structure").get("bravais_index")
+		if self.bravais_n in bravais_index:
+			self.bravais = bravais_index[ self.bravais_n]
+		self.bravais_n = int( self.bravais_n)
 		self.lp      = float(root.find("output//atomic_structure").get("alat"))
-		self.a= list( map( lambda y: list(map( float, y)), \
+		self.a = list( map( lambda y: list(map( float, y)),
 			map( lambda x: x.text.split(" "), root.find("output//cell").getchildren())))
-		self.b= list( map( lambda y: list(map( float, y)), \
+		self.b = list( map( lambda y: list(map( float, y)),
 			map( lambda x: x.text.split(" "), root.find("output//reciprocal_lattice").getchildren())))
-		self.atoms=list( map( lambda x: {'name':x.get("name"), 'i':int(x.get("index")), \
-			'coord':list(map( float, x.text.split(" ")))}, root.findall("output//atom")))
+		self.atoms = list( map( lambda x: {
+			'name':x.get("name"), 
+			'i':int(x.get("index")),
+			'coord':list(map( float, x.text.split(" ")))
+			}, root.findall("output//atom")
+			))
+		node = root.find("input/atomic_species")
+		self.atom_spec_n = int( node.get("ntyp"))
+		self.atom_spec = list( map( lambda x: {
+			'mass':float(x.find("mass").text),
+			'name':x.get("name"),
+			'pfile':x.find("pseudo_file").text,
+			'sm':float(x.find("starting_magnetization").text)
+			},node.getchildren()
+			))
 		
 		self.symm = []
 		for node in root.findall( "output//symmetry"):
