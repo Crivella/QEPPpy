d3bd2e384af1360459bf50a756d56530f3e5a278
Crivella <davidecrivella@yahoo.it>
2018-11-11 21:54:54 +0100

    Implemented computation of charge density.

--- qepppy/classes/qe_wfc.py
+++ qepppy/classes/qe_wfc.py
@@ -50,9 +50,12 @@ class qe_wfc( qbr):
 		a2 = np.cross( self.recipr[2], self.recipr[0])
 		a3 = np.cross( self.recipr[0], self.recipr[1])
 		cell = np.array( (a1,a2,a3)) * 2*np.pi / vol
+		#cell = np.array([[1,0,0],[0,1,0],[0,0,1]]) * 10.2 
 
-		div = np.array( (x,y,z))
+		div = np.array( (x-1,y-1,z-1))
 		delta  = cell / div[:,None]
+		delta = delta.transpose()
+		recipr = self.recipr.transpose()
 
 		try:
 			weight = self.weight
@@ -60,7 +63,7 @@ class qe_wfc( qbr):
 			logger.warning( "Weight data for kpt #{} missing... settting it to 1".format( self.kpt_num))
 			weight = 1
 
-		gvect_cart = np.array( [np.dot( self.recipr, G) for G in self.gvect])
+		gvect_cart = np.array( [np.dot(recipr, G) for G in self.gvect])
 
 		grid = np.empty( (x,y,z,3))
 		dens = np.empty( (x,y,z))
@@ -78,7 +81,7 @@ class qe_wfc( qbr):
 						cc = 0
 						for ng, G in enumerate( gvect_cart):
 							exp = np.dot( G+self.kpt, coord)
-							cc += coeff[ng] * np.exp( exp)
+							cc += coeff[ng] * np.exp( 1j * exp)
 						dens[n1][n2][n3] = (cc.real**2 + cc.imag**2)
 
 		dens *= weight / vol
@@ -88,24 +91,29 @@ class qe_wfc( qbr):
 		from mayavi import mlab
 		src = mlab.pipeline.scalar_field( dens)
 		print( src)
-		mlab.pipeline.iso_surface( src)
+		mlab.pipeline.iso_surface( src, contours=[dens.max()/3, ], opacity=0.3)
 		mlab.show()
-		"""
+		#"""
 
-		import matplotlib.pyplot as plt
-		from mpl_toolkits.mplot3d import Axes3D
+		if plot:
+			import matplotlib.pyplot as plt
+			from mpl_toolkits.mplot3d import Axes3D
 
-		fig = plt.figure()
-		ax = fig.add_subplot( 111, projection='3d')
+			fig = plt.figure()
+			for i in range( z):
+				ax = fig.add_subplot( 1,z,i+1 , projection='3d')
+				ax.set_title( "z slice #{}".format( i))
+				ax.view_init( 90, 0)
+				ax.margins( tight=True)
+				ax.set_zticks([])
 
-		#print( grid[:,:,:,0])
-		#dens.shape = (343,)
-		#dens /= dens.max()
-		#print( dens)
-		#ax.scatter ( grid[:,:,:,0], grid[:,:,:,1], grid[:,:,:,2] , c=dens, cmap='Spectral')
-		ax.plot_surface( grid[:,:,0,0], grid[:,:,0,1], dens[:,:,0])
+				ax.xaxis.pane.fill = False
+				ax.yaxis.pane.fill = False
+				ax.zaxis.pane.fill = False
+				ax.grid( False)
+				ax.plot_surface( grid[:,:,i,0], grid[:,:,i,1], dens[:,:,i], cmap='inferno')
 
-		plt.show()
+			plt.show()
 		return dens
 
 
