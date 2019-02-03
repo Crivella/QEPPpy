9168af7a85b950f125c7d41c146462eb45f8cced
Crivella <davidecrivella@yahoo.it>
2019-02-03 18:20:50 +0100

    Imporved style and execution in tools 'sum_pdos.py' and 'pdos_char.py'.

--- qepppy/tools/pdos_char.py
+++ qepppy/tools/pdos_char.py
@@ -1,25 +1,23 @@
 #! python
 
-import os.path
+import os
 import re
 
-def pdos_char( fname="", kpt_list=[], bnd_list=[], thr=0):
-	if( not os.path.isfile(fname)):
-		print( "ERROR: File fname:'{}' does not exist.".format( fname))
-		return 1
-	if( not kpt_list):
-		print( "ERROR: Passing empty 'kpt_list'.")
-		return 1
-	if( isinstance( kpt_list, str)):
-		kl = list( map( int, kpt_list.split(",")))
-	if( isinstance( kpt_list, list)):
+def pdos_char(fname="", kpt_list=[], bnd_list=[], thr=0):
+	if not os.path.isfile(fname):
+		raise FileNotFoundError("File fname:'{}' does not exist.".format(fname))
+	if   isinstance(kpt_list, str):
+		kl = list(map(int, kpt_list.split(",")))
+	elif isinstance(kpt_list, list):
 		kl = kpt_list
-	if( isinstance( bnd_list, str)):
-		bl = list( map( int, bnd_list.split(",")))
-	if( isinstance( bnd_list, list)):
+	else:
+		raise ValueError("'kpt_list' must be either a list or a string(with elements eparated by a comma")
+	if   isinstance(bnd_list, str):
+		bl = list(map(int, bnd_list.split(",")))
+	elif isinstance(bnd_list, list):
 		bl = bnd_list
-	if( len( bl) > 0):
-		cb = True
+	else:
+		raise ValueError("'bnd_list' must be either a list or a string(with elements eparated by a comma")
 
 	state_l = []
 
@@ -28,73 +26,64 @@ def pdos_char( fname="", kpt_list=[], bnd_list=[], thr=0):
 	ck = False
 	ce = False
 	cp = False
-	cb = False
-	if( len( bl) > 0):
-		cb = True
-	for line in open( fname):
-		line = line.rstrip()
-		if( " state #" in line):
-			state_l.append( line.split(":")[1])
-		if( " k = " in line or ck):
-			if( " k = " in line):
-				k+=1
-				b=-1
-				kpt = list( map( float, list( filter( None, line.split( " ")))[2:5]))
-				ck = True
-				ce = False
-				if( k+1 not in kl):
-					ck = False
+	with open(fname) as f:
+		for line in f:
+			line = line.rstrip()
+			if " state #" in line:
+				state_l.append(line.split(":")[1])
+			if " k = " in line:
+				k  += 1
+				b   = -1
+				ce  = False
+				kpt = list(map(float, list(filter(None, line.split(" ")))[2:5]))
+				if k+1 in kl:
+					ck = True
+					print("KPT (#{:5d}):\t{}".format(k+1, kpt))
 				else:
-					print( "KPT (#{:5d}):\t{}".format( k+1, kpt))
-			if( ck and (" e(" in line or " e =" in line)):
-				if( " e(" in line):
+					ck = False
+			if ck and (" e(" in line or " e =" in line):
+				if " e(" in line:
 					off=4
 				else:
 					off=2
 				ce = True
-				b+=1
+				b +=1
 				
-				#print( line, line.split( " "), list( filter( None, line.split( " "))))
-				el = float( list( filter( None, line.split( " ")))[off])
-				if( cb and b+1 not in bl):
-					ce = False
+				el = float(list(filter(None, line.split(" ")))[off])
+				if b+1 in bl:
+					print("\tBND (#{:3d}):\t{} eV".format(b+1, el))
 				else:
-					print("\tBND (#{:3d}):\t{} eV".format( b+1, el))
+					ce = False
 					
-			if( cp and " |psi|^2" in line):
+			if cp and " |psi|^2" in line:
 				cp = False
-			if( ce and (" psi = " in line or cp)):
+			if ce and (" psi = " in line or cp):
 				cp = True
-				l = list( filter( None, re.split( " +psi = | +\+|\*\[#|\]\+", line)))
-				#print( l[1::2], l[0::2])
-				for s, wf in zip( l[1::2], l[0::2]):
-					wf=float(wf)
-					if( wf>thr):
-						print( "\t\t{}:\t{:7.3f}%".format( state_l[int(s)-1], wf*100))
-
-
-	return 0
-
+				l = list(filter(None, re.split(r" +psi = | +\+|\*\[#|\]\+", line)))
+				for s, wf in zip(l[1::2], l[0::2]):
+					wf = float(wf)
+					if wf >= thr:
+						print("\t\t{}:\t{:7.3f}%".format(state_l[int(s)-1], wf*100))
 
 
 if __name__ == "__main__":
 	import sys
-	argc = len( sys.argv)
-	if( not 2<=argc<=5):
+	argc = len(sys.argv)
+	if not 2<=argc<=5:
 		print("Incorrect use. Pleas pass arguments:"
 			"\n\t'fname',"
 			"\n\t'kpt_list\t(comma separated)',"
 			"\n\t'bnd_list\t(optional) (comma separated)'"
 			"\n\t'thr\t(optional) (threshold for printing percantages)'")
 		exit()
-	if( argc==2):
-		pdos_char( sys.argv[1])
-	if( argc==3):
-		pdos_char( sys.argv[1], sys.argv[2])
-	if( argc==4):
-		pdos_char( sys.argv[1], sys.argv[2], sys.argv[3])
-	if( argc==5):
-		pdos_char( sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))
+	if argc==2:
+		pdos_char(sys.argv[1])
+	elif argc==3:
+		pdos_char(sys.argv[1], sys.argv[2])
+	elif argc==4:
+		pdos_char(sys.argv[1], sys.argv[2], sys.argv[3])
+	elif argc==5:
+		pdos_char(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))
 
 
 
--- qepppy/tools/sum_pdos.py
+++ qepppy/tools/sum_pdos.py
@@ -1,65 +1,64 @@
-from os      import listdir
-from os.path import isfile, join
+import os
+import numpy as np
 from re      import split
-from numpy   import array, zeros, loadtxt, savetxt
 
+orb_types = ('s', 'p', 'd', 'f') 
+max_orb = len(orb_types)
 
+def sum_pdos(atoms="", path=".", plot=False, pfile=True):
+	if not atoms:
+		raise ValueError("Passing empty name for 'atoms'")
 
-orb_types = ( 's', 'p', 'd', 'f') 
-max_orb = len( orb_types)
+	if isinstance(atoms, str):
+		atoms_l = atoms.split(",")
+	elif isinstance(atoms, list):
+		atoms_l = atoms
+	else:
+		raise NotImplementedError()
+	max_atm = len(atoms_l)
 
-def sum_pdos( atoms="", path=".", plot=False, pfile=True):
-	if( not atoms):
-		print( "Passing empty name for 'atoms'")
-		return 1
+	res_orb = None
+	for j,atom in enumerate(atoms_l):
+		for f in os.listdir(path):
+			if "pdos_atm#" in f and "({})".format(atom) in f:
+				name = os.path.join(path, f)
+				print(name)
+				print("READING: {}".format(name))
+				data = np.loadtxt(fname=name, comments="#")
 
-	check = False
-	n_pt = 0
-	atoms_l = atoms.split(",")
-	max_atm = len( atoms_l)
-
-	for atom, j in zip( atoms_l, range( len( atoms_l))):
-		for f in listdir( path):
-			if( "pdos_atm#" in f and "({})".format( atom) in f):
-				name = join( path, f)
-				print( "READING: {}".format( name))
-				data = loadtxt( fname=name, comments="#")
-
-				n_pt = len( data)
-				if( not check):
-					check = True
-					#res_x   = data[:,0]
-					res_orb = zeros(( max_atm, n_pt, max_orb+1))
+				if res_orb is None:
+					n_pt = data.shape[0]
+					res_orb = np.zeros((max_atm, n_pt, max_orb+1))
 					res_orb[0:max_atm,:,0] = data[:,0]
-				l = list(filter( None, split( ".+atm#|\(.+#|\(|\)|\_j", f)))
+				l = list(filter(None, split(r".+atm#|\(.+#|\(|\)|\_j", f)))
 				atm_n = j
-				wfc_n = int( l[1])-1
+				wfc_n = int(l[1])-1
 				try:
-					orb_n = int( orb_types.index( l[2]))
+					orb_n = int(orb_types.index(l[2]))
 				except ValueError:
-					print( "Non recognize orbital type {}".format( l[2]))
+					print("Non recognize orbital type {}".format(l[2]))
 					return 1
 
 				res_orb[atm_n,:,orb_n+1] += data[:,1]
 
-	if( check):
-		if( pfile):
-			for atom, j in zip( atoms_l, range( len( atoms_l))):
-				fname = "{}_total_pdos_orb.dat".format( atom)
-				head = "{:>11}".format( "Energy(eV)") + \
-					"".join('{:>12}_{{}}'.format(atom) * max_orb).format( *orb_types)
-				savetxt( fname=fname, X=res_orb[j,:,:], fmt="%13.8f"+"".join('%14g'*max_orb), header=head)
+	if not res_orb is None:
+		if pfile :
+			for atom, j in zip(atoms_l, range(len(atoms_l))):
+				fname = "{}_total_pdos_orb.dat".format(atom)
+				head = "{:>11}".format("Energy(eV)") + \
+					"".join('{:>12}_{{}}'.format(atom) * max_orb).format(*orb_types)
+				np.savetxt(fname=fname, X=res_orb[j,:,:], fmt="%13.8f"+"".join('%14g'*max_orb), header=head)
 
 
 		if plot:
 			import matplotlib.pyplot as plt
 			from matplotlib.ticker import AutoMinorLocator as AML
 			fig, ax = plt.subplots()
-			for atom, j in zip( atoms_l, range( len( atoms_l))):
-				for y, label in zip( res_orb[j,:,1:max_orb+1].transpose(), orb_types):
-					plt.plot( res_orb[j,:,0], y, label="{}_{}".format( atom, label))
-			plt.ylabel( "PDOS arb. units")
-			plt.xlabel( "Energy( eV)")
+			for atom, j in zip(atoms_l, range(len(atoms_l))):
+				for y, label in zip(res_orb[j,:,1:max_orb+1].transpose(), orb_types):
+					plt.plot(res_orb[j,:,0], y, label="{}_{}".format(atom, label))
+			plt.ylabel("PDOS arb. units")
+			plt.xlabel("Energy(eV)")
 			ml1 = AML(5)
 			ml2 = AML(5)
 			ax.yaxis.set_minor_locator(ml1)
@@ -68,43 +67,42 @@ def sum_pdos( atoms="", path=".", plot=False, pfile=True):
 			ax.xaxis.set_tick_params(which='both', top = True)
 			plt.legend()
 			plt.show()
-		
-	return 0
 
+		return res_orb
 '''
-		for atom, j in zip( atoms_l, range( len( atoms_l))):
-			fout2 = open( "{}_total_pdos_orb.dat".format( atom), "w")
-			fout2.write( "#{:>12}".format( "Energy(eV)"))
-			for i in range( max_orb):
-				fout2.write( "{:>12}_{}".format( atom, orb_types[i]))
-			fout2.write( "\n")
-			for i in range( n_pt):
-				fout2.write( "{:13.8f}".format( res_x[i]))
+		for atom, j in zip(atoms_l, range(len(atoms_l))):
+			fout2 = open("{}_total_pdos_orb.dat".format(atom), "w")
+			fout2.write("#{:>12}".format("Energy(eV)"))
+			for i in range(max_orb):
+				fout2.write("{:>12}_{}".format(atom, orb_types[i]))
+			fout2.write("\n")
+			for i in range(n_pt):
+				fout2.write("{:13.8f}".format(res_x[i]))
 				for item in res_orb[j,i,:]:
-					fout2.write( "{:14g}".format( item))
-				fout2.write( "\n")
+					fout2.write("{:14g}".format(item))
+				fout2.write("\n")
 			fout2.close()
 '''
 
 
 if __name__ == "__main__":
 	import sys
-	argc = len( sys.argv)
-	if( not 2<=argc<=5):
+	argc = len(sys.argv)
+	if(not 2<=argc<=5):
 		print("Incorrect use. Please pass arguments:"
 			"\n\t'atom_list\t(comma separated)',"
 			"\n\t'path\t\t(default = \".\")',"
 			"\n\t'plot\t\t(default = False)'"
 			"\n\t'pfile\t\t(default = True) (print output file)")
 		exit()
-	if( argc==2):
-		sum_pdos( sys.argv[1])
-	if( argc==3):
-		sum_pdos( sys.argv[1], sys.argv[2])
-	if( argc==4):
-		sum_pdos( sys.argv[1], sys.argv[2], bool( sys.argv[3]))
-	if( argc==5):
-		sum_pdos( sys.argv[1], sys.argv[2], bool( sys.argv[3]), bool( sys.argv[4]))
+	if(argc==2):
+		sum_pdos(sys.argv[1])
+	if(argc==3):
+		sum_pdos(sys.argv[1], sys.argv[2])
+	if(argc==4):
+		sum_pdos(sys.argv[1], sys.argv[2], bool(sys.argv[3]))
+	if(argc==5):
+		sum_pdos(sys.argv[1], sys.argv[2], bool(sys.argv[3]), bool(sys.argv[4]))
 
 
 
