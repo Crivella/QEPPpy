10133e526d152833d18439875513488cc9433b81
Crivella <davidecrivella@yahoo.it>
2019-02-19 21:25:32 +0100

    Improvements to input_files.py

--- qepppy/qe/parser/input_files.py
+++ qepppy/qe/parser/input_files.py
@@ -3,10 +3,10 @@ from .templates import template_parser as templ
 from ...logger import logger, error
 
 #import logging
-#logger = logging.getLogger( __name__)
-#logging.basicConfig( format='%(levelname)s: %(name)s\n%(message)s\n')
+#logger = logging.getLogger(__name__)
+#logging.basicConfig(format='%(levelname)s: %(name)s\n%(message)s\n')
 
-def trim_ws( str):
+def trim_ws(str):
 	"""
 	Trim all withspace not included in a string
 	"""
@@ -26,8 +26,8 @@ def trim_ws( str):
 
 	trim_ws
 
-# logger()( )
-class input_files( templ):
+# logger()()
+class input_files(templ):
 	"""
 	Class to handle any QE input (after loading the proper template).
 	Supposed to be used as a parent class for child specific to the qe file.
@@ -35,7 +35,7 @@ class input_files( templ):
 	 - parse = Name of the file to parse
 	 - templ_name = Name of the template file to use
 	"""
-	def __init__( self, templ_file="", parse="", **kwargs):
+	def __init__(self, templ_file="", parse="", **kwargs):
 		try:
 			self.templ_file
 		except AttributeError as e:
@@ -43,43 +43,43 @@ class input_files( templ):
 		if templ_file:
 			self.templ_file = templ_file
 		if not self.templ_file:
-			raise error( "Must give a template file.\n")
-		self.load_templ( self.templ_file)
+			raise error("Must give a template file.\n")
+		self.load_templ(self.templ_file)
 
-		#parse = kwargs.get( 'parse')
+		#parse = kwargs.get('parse')
 		if parse:
-			self.parse_input( parse=parse)
+			self.parse_input(parse=parse)
 		else:
 			pass
-			#logger.warning( "Creating instance of 'qe_in' without parsing any input file.")
+			#logger.warning("Creating instance of 'qe_in' without parsing any input file.")
 
 		#Check if initialization keyword arguments are compliant with the given namelist template
 		for nl, v in kwargs.items():
-			if not isinstance( v, dict):
+			if not isinstance(v, dict):
 				continue
-				#raise Exception( "Invalid kwargs.\n{}".format( kwargs))
+				#raise Exception("Invalid kwargs.\n{}".format(kwargs))
 			for k, v1 in v.items():
-				self.set_nl( nl, k, v1)
+				self.set_nl(nl, k, v1)
 
-		super().__init__( **kwargs)
+		super().__init__(**kwargs)
 		return
 
-	def __str__( self):
+	def __str__(self):
 		return self.convert()
 
-	def print_input( self, parse="", check=True):
+	def print_input(self, parse="", check=True):
 		"""
 		Print this or a child class __str__() to a file or to the stdout
 		If check, validate the file before printing it
 		"""
 		if parse:
-			f = open( parse, "w+")
+			f = open(parse, "w+")
 		else:
 			f = sys.stdout
 
 		if check:
 			self.validate()
-		f.write( self.__str__())
+		f.write(self.__str__())
 
 		if parse: 
 			f.close()
@@ -87,12 +87,12 @@ class input_files( templ):
 		return
 
 
-	def parse_input( self, parse=""):
+	def parse_input(self, parse=""):
 		"""
 		Read an input file and load it into the template values.
 		"""
 		if not parse:
-			raise Exception( "Must pass a filename to open")
+			raise Exception("Must pass a filename to open")
 
 		#Read all the file content into 'content'
 		with open(parse) as f:
@@ -102,47 +102,61 @@ class input_files( templ):
 		card = None
 		for l in content:
 			#Ignore comments
-			ls = l.strip().split( "!")[0]
+			ls = l.strip()
 			if not ls: 
 				continue
 			#CASE: Namelist name
 			if '&' == ls[0]:
 				nl = ls[1:].upper()
-				if not self.check_nl( nl):
-					raise Exception( "Reading unrecognized namelist '{}'".format( nl))
-			#CASE other
+				if not self.check_nl(nl):
+					raise Exception("Reading unrecognized namelist '{}'".format(nl))
+				continue
+			if '/' == ls:
+				if not nl:
+					raise Exception("Terminating namelist before opening it ?!")
+				nl = None
+				continue
+
+			if nl:
+				print(ls)
+				ls = ls.split("!")[0].strip()
+				if not ls:
+					continue
+				print(ls)
+				print()
+				#(not '/' in l or '=' in l) => Recognize namelist field from other fields or namelist end '/'
+				if not '=' in ls:
+					raise Exception("Invalid syntax '{}'".format(l))
+					#if not nl: raise Exception("Corrupted input file:\n{}".format(l))
+				#Read namelist fields separated by endline ('\n') or by commas (',')
+				for e in filter(None, ls.split(",")):
+					l1 = trim_ws(e).split("=")
+					if len(l1) != 2: 
+						raise Exception("Corrupt input file:\n{}".format(l))
+					v = l1[1].replace("\"", "").replace("'", "")
+					
+					#Check if the field/parameter name is present in the namelist template
+					self.set_nl(nl=nl, k=l1[0], v=v)
+				continue
+			#Case reading a card
 			else:
-				#nl => Recognize if reading field outside of namelist
-				if nl:
-					#(not '/' in l or '=' in l) => Recognize namelist field from otehr fields or namelist end '/'
-					if not '/' in ls or '=' in ls:
-						#if not nl: raise Exception( "Corrupted input file:\n{}".format( l))
-						#Read namelist fields separated by endline ('\n') or by commas (',')
-						for e in filter( None, ls.split( ",")):
-							l1 = trim_ws(e).split( "=")
-							if len(l1) != 2: 
-								raise Exception( "Corrupt input file:\n{}".format( l))
-							v = l1[1].replace("\"", "").replace("'", "")
-							
-							#Check if the field/parameter name is present in the namelist template
-							self.set_nl( nl=nl, k=l1[0], v=v)
-					else:
-						nl = None
-				#Case reading a card
+				# lt = ' '.join(filter(None, ls.replace("{", "").replace("}", "").split(" ")))
+				# lt = lt.split(" ")
+				ls = ls.split("#")[0].strip()
+				lt = list(filter(None, ls.replace("{", "").replace("}", "").split(" ")))
+				if self.check_card(lt[0]):
+					card=lt[0]
+					try: 
+						v = lt[1]
+					except: 
+						v = None
+					self.set_card(card=card, v=v)
+					#print(card, v)
 				else:
-					lt = ' '.join( filter( None, ls.replace( "{", "").replace( "}", "").split( " ")))
-					lt = lt.split( " ")
-					if self.check_card( lt[0]):
-						try: 
-							v = lt[1]
-						except: 
-							v = None
-						card=lt[0]
-						self.set_card( card=card, v=v)
-						#print( card, v)
-					else:
-						self.set_card( card=card, el=ls)
-		return
+					self.set_card(card=card, el=ls)
+				continue
+
+			# raise Exception("Invalid syntax '{}'".format(l))
 
 
 
