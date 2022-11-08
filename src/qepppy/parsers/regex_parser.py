f87cc6f7d98b6864bb734292739e98885c817005
Crivella <davidecrivella@yahoo.it>
2022-11-08 12:02:26 +0100

    Added documentation to regex parser

--- src/qepppy/parsers/regex_parser.py
+++ src/qepppy/parsers/regex_parser.py
@@ -13,6 +13,54 @@ matches = {
 # 	print(*args, **kwargs)
 
 class Parser_regex():
+	"""
+	Parse a file using regex defined syntax. Meant to be used as a child class.
+	The parent class can be passed a `regex_data` on its init to specify the
+	syntax:
+	 - KEYs: name of the attribute/s to be set in the class after parsing the 
+	   file. Can use comma to specify more than one attribute to be set as a
+	   result of the same parse operation. '_' can be used in order to ignore
+	   part of the results
+	   EG: 
+	   - {'test':...} will set an attribute `test` on the parent class 
+	   - {'test1,test2':...} will set attributes `test1` and `test2`.
+	   - {'p1,_,p2':...} will set attributes `p1` and `p2`, while ignoring the
+	     second variable resulting from the parse operation. 
+	   For more info on how the result is split from the parsing see the parse
+	   rules section.
+	   with the result of the parsing rule given by the value.
+	 - VALUEs: dictionary specifing a set of rules to be used by the parser:
+	   - rstring: REGEX string used to parse the file
+	   - typ: specify the expected resulting type for the attribute.
+	          Several types are implemented for a 'single_value' or 'iterable'
+			  reult. If a non-implemented type is used a NotImplemented
+			  exception will be risen
+			  - single_value: int/float/str/bool
+			    This search expect the REGEX string to contain a 'flag' named
+				search group, the content of which will be the result of the 
+				parse. If 'flag' is not present, a default search group will
+				be added depending on the specified type.
+				EG: rstring='nbnd = ', typ='int
+				    final_rstring = 'nbnd = [\s]*(?P<flag>[\d\-]+)'
+			  - iterable: list/np.ndarray
+			    In this case rstring can specify multiple search groups, the
+				result of which will be made into a list.
+				- If only an attribute name is given, the result will be a 2D
+				  list/array, where the each row is the result of a `finditer`
+				  and each column will correspond to a search group.
+				- If multiple names are given, every column will be assigned
+				  to an attribute in the corresponding order.
+				  The number of columns/rgx_groups has to be equal to the number
+				  of attribute names, or max_num has to be used to truncate
+				  the resulting array.
+	   - max_num: MAX number of column to consider when parsing iterables
+	     Can be positive/negative to truncate starting from the left/right 
+		 edge.
+	   - re_scale_fact: Multiply the final result by this scale factor.
+	     If this is a string, the parser will look for the corresponding
+		 attribute and get its value (useful when the factor has to be parsed
+		 as well)
+	"""
 	def __init__(self, *args, file=None, regex_data={}, **kwargs):
 		super().__init__(*args, **kwargs)
 
